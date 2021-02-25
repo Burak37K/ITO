0482ba2cc14138a6ebe4c204f1c4790245d3128e
burak <burak_t90-@hotmail.de>
2021-02-25 23:09:25 +0100

    working w pytorch

--- MachineLearning/NeuronaleNetze.py
+++ MachineLearning/NeuronaleNetze.py
@@ -1,6 +1,27 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-x = torch.Tensor(3,3)
+class myNet(nn.Module):
+    def __init__(self):
+        super(myNet, self).__init__()
+        self.lin1 = nn.Linear(3,3)
+        self.lin2 = nn.Linear(3,3)
 
-print(x)
+
+    def forward(self, x):
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
+        return x
+
+    def num_flat_features(self,x):
+        size = x.size()[1:]
+        num = 1
+        for i in size:
+            num *= 1
+
+
+netz = myNet()
+
+print(netz)
 
